26ee468f508110368bd729eec57183d44f531620
Guthrie McAfee Armstrong <accounts@gmarmstrong.dev>
2023-07-04 17:00:15 +0000

    refactor!: replace template project metadata

--- src/main/java/dev/gmarmstrong/magicpenny/ExampleMod.java
+++ src/main/java/dev/gmarmstrong/magicpenny/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.example_mod;
+package dev.gmarmstrong.magicpenny;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
@@ -9,10 +9,10 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("MagicPenny");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		LOGGER.info("Hello, Quilt world, from {}!", mod.metadata().name());
 	}
 }
--- src/main/java/dev/gmarmstrong/magicpenny/mixin/TitleScreenMixin.java
+++ src/main/java/dev/gmarmstrong/magicpenny/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package dev.gmarmstrong.magicpenny.mixin;
 
-import com.example.example_mod.ExampleMod;
+import dev.gmarmstrong.magicpenny.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
